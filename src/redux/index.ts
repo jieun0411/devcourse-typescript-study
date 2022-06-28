9fa126b76d2693ea990a8a6abc96f9eac9c1c4c7
Jieun Kim <67778677+jieun0411@users.noreply.github.com>
2022-06-28 17:14:34 +0900

    add: redux-logger

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,9 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import { tasks } from './tasks';
+import logger from "redux-logger"
 
 const rootReducer = combineReducers({tasks});
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(logger))
 
 export type RootState = ReturnType<typeof rootReducer>;
