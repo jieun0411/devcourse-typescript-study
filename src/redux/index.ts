4c0f4dab610595812c226e46b5f919c94a4c8534
Jieun Kim <67778677+jieun0411@users.noreply.github.com>
2022-06-28 17:20:39 +0900

    add: redux-devtools-extension

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,9 +1,13 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { tasks } from './tasks';
 import logger from "redux-logger"
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({tasks});
 
-export const store = createStore(rootReducer, applyMiddleware(logger))
+export const store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(logger))
+);
 
 export type RootState = ReturnType<typeof rootReducer>;
